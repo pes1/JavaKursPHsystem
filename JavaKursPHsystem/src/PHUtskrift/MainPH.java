7c8344b54b60c6b37ffccd8be6590befb7276898
User (PES) <entermosjava@gmail.com>
2016-06-20 13:04:08 +0200

    test igen.

--- JavaKursPHsystem/src/PHUtskrift/MainPH.java
+++ JavaKursPHsystem/src/PHUtskrift/MainPH.java
@@ -3,7 +3,8 @@ package PHUtskrift;
 public class MainPH {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world !");
+		System.out.println("Hello world  !");
+		System.out.println("Hello world  !");
   
 	} 
 
