9676baeaef0e87c5959df74629127742e31abb04
User (PES) <entermosjava@gmail.com>
2016-06-20 13:06:42 +0200

    Ännu en test !

--- JavaKursPHsystem/src/PHUtskrift/MainPH.java
+++ JavaKursPHsystem/src/PHUtskrift/MainPH.java
@@ -5,6 +5,7 @@ public class MainPH {
 	public static void main(String[] args) {
 		System.out.println("Hello world  !");
 		System.out.println("Hello world  !");
+		System.out.println("Hello world  !");
   
 	} 
 
