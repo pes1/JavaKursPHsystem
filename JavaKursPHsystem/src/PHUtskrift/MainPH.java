4a2ade9048642ba22819935093da646e74bc5f2e
User (PES) <entermosjava@gmail.com>
2016-06-20 13:08:15 +0200

    Fyra testrader !

--- JavaKursPHsystem/src/PHUtskrift/MainPH.java
+++ JavaKursPHsystem/src/PHUtskrift/MainPH.java
@@ -6,6 +6,7 @@ public class MainPH {
 		System.out.println("Hello world  !");
 		System.out.println("Hello world  !");
 		System.out.println("Hello world  !");
+		System.out.println("Hello world  !");
   
 	} 
 
