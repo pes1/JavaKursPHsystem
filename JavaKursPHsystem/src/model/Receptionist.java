2d704c9916030ba5f6086987e292353fa73826d9
larslarslindholm <lindholm.edu@gmail.com>
2016-06-21 16:23:51 +0200

    Now it's time to employee an receptionist

---
+++ JavaKursPHsystem/src/model/Receptionist.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Receptionist extends Anställd {
+
+	//Variables
+	private static long basMånadsLön   = 22000;
+	private static long basMånadsBonus = 1000;
+	
+	private int[] betyg = new int[10];
+	private int nästaBetyg = 0;
+	private double kundnöjdhet  = 2;  
+	//TODO: medel av 10 sista nöjdhetsrapporterna med betyg 1-5
+
+	public Receptionist(String namn) {
+		super(namn);
+	}
+	
+	@Override
+	public long getBasMånadsLön() {
+		return basMånadsLön;
+	}
+
+	@Override
+	public long getBasMånadsBonus() {
+		return basMånadsBonus;
+	}
+
+	public double getKundnöjdhet() {
+		return kundnöjdhet;
+	}
+
+	@Override
+	public long getBonus() {
+		return (int)kundnöjdhet * basMånadsBonus;
+	}
+
+	@Override
+	public long getMånadsLön() {
+		return (basMånadsLön + getBonus());
+	}
+	
+	// Sätt ett nytt betyg som skriver över det älsta betyget, 
+	// samt inkrementera variabeln för älsta betyg
+	public void setBetyg(int nyttBetyg) {
+		betyg [nästaBetyg++] = nyttBetyg;
+		nästaBetyg = nästaBetyg % betyg.length;
+	}
+	
+	public double calcKundnöjdhet(){
+		return calcMedel(betyg);
+	}
+		
+	public double calcMedel(int[] heltal) {
+		int summa = 0;
+		if (heltal.length == 0) return 0;
+		for (int i = 0; i < heltal.length; i++) {
+			summa += heltal[i];	
+		}
+		return (double) summa / (double) heltal.length;
+	}
+}
---
+++ JavaKursPHsystem/src/model/TestReceptionist.java
@@ -0,0 +1,30 @@
+package model;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestReceptionist {
+
+	String testNamn = "ReceptionsNisse";
+
+	@Test
+	public void test() {
+		Receptionist r = new Receptionist("testNamn");
+		
+		assertTrue(r.getNamn() == "testNamn"); //dvs inte "ReceptionsNisse"		
+		assertTrue(r.getBasMånadsLön() == 22000);
+		assertTrue(r.getBasMånadsBonus() == 1000);
+		assertTrue(r.getKundnöjdhet() == 2);
+		assertTrue(r.getBonus() == 2000);		
+		assertTrue(r.getMånadsLön() == 24000);
+		
+		r.setBetyg(2);
+		assertTrue(r.calcKundnöjdhet() == 0.2);
+		r.setBetyg(4);
+		assertTrue(r.calcKundnöjdhet() == 0.6 );
+		for (int i= 0; i<10; i++){
+			r.setBetyg(5);
+		}
+		assertTrue(r.calcKundnöjdhet() == 5);
+	}
+}
+
