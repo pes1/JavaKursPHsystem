c40aca16a6c713724168bee759727bd09d7d7e9b
Magnus Forsberg <magnus.forsberg@gmail.com>
2016-06-21 11:14:56 +0200

    Raderade bortkommenterad kod

--- JavaKursPHsystem/src/model/Tekniker.java
+++ JavaKursPHsystem/src/model/Tekniker.java
@@ -1,63 +1,49 @@
 package model;
 
 public class Tekniker extends Anställd {
-	
+
 	//Variables
 	private static long basMånadsLön   = 27000;
 	private static long basMånadsBonus = 2000;
-	
-//	private String namn;
+
 	private double uppTid = 1.0;
-	
+
 	//Constructor
 	public Tekniker(String namn) {
-//		this.namn = namn;
 		super(namn);  				//-- Anropar överklassens konstruktor.
-
 	}
-	
+
 	//Methods ---------------------------------------------------
-	
+
 	@Override
 	public String toString() {
 		return "Tekniker " + getNamn();
 	}
-	
+
 	@Override
 	public long getBonus() {
 		return (long) (((double) getBasMånadsBonus()) *  getUppTid());
 	}
-	
+
 	@Override
 	public long getBasMånadsBonus(){
 		return basMånadsBonus;
 	}
-	
+
 	@Override
 	public long getBasMånadsLön(){
 		return basMånadsLön;
 	}
 
-	
-	
 	@Override
 	public long getMånadsLön() {
 		return getBasMånadsLön() +  getBonus();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	public double getUppTid() {
 		return uppTid;
 	}
-	
+
 	public void setUppTid(double nyUppTid) {
 		if(nyUppTid > 1.0) {
 			this.uppTid = 1.0;
@@ -69,5 +55,5 @@ public class Tekniker extends Anställd {
 			this.uppTid = nyUppTid;
 		}
 	}
-	
+
 }//Class
