253ff46c47463227b11151b9fb5acc36f651c497
Magnus Forsberg <magnus.forsberg@gmail.com>
2016-06-21 11:25:22 +0200

    Ny testklass TestAnställd

---
+++ JavaKursPHsystem/src/TestAnställd.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.Anställd;
+import model.Tekniker;
+
+public class TestAnställd {
+
+	@Test
+	public void test() {
+		Anställd anställd = new Tekniker("Kalle");
+		
+	}
+
+}
