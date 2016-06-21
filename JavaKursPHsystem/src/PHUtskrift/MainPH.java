c8b741f2b18efee2e7cfe42df3d98ed3f24b9b4e
Magnus Forsberg <magnus.forsberg@gmail.com>
2016-06-21 14:09:05 +0200

    Rudimentärt CLI i main

--- JavaKursPHsystem/src/PHUtskrift/MainPH.java
+++ JavaKursPHsystem/src/PHUtskrift/MainPH.java
@@ -1,22 +1,44 @@
 package PHUtskrift;
 
-import model.Tekniker;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class MainPH {
 
-	public static void main(String[] args) {
-		
-		Tekniker tekniker = new Tekniker("Kalle");
-		
-		tekniker.setUppTid(.999);
 
-		System.out.println("Namn: "           + tekniker.getNamn());
-		System.out.println("upptid: "         + tekniker.getUppTid());
-		System.out.println("basmånadsbonus: " + tekniker.getBasMånadsBonus());
-		System.out.println("bonus: "          + tekniker.getBonus());
-		System.out.println("basmånadsön: "    + tekniker.getBasMånadsLön());
-		System.out.println("slutlön: "        + tekniker.getMånadsLön());
-	} 
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int input = -1;
+
+		while(input != 0) {
+			System.out.println("\nVälj ett kommando:");
+			System.out.println("==================");
+
+			System.out.println("1. Skapa ny anställd");
+
+			System.out.println("\n0. Avsluta");
+
+			try{
+				input = scanner.nextInt();
+			} catch (InputMismatchException ime) {
+				System.out.println("input: " + input);
+				System.out.println("Endast siffror.");
+				
+			}
+
+			scanner.nextLine();
+			System.out.println(input);
+
+		}//while
+
+		System.out.println("\nProgrammet avslutas.");
+		scanner.close();
+		System.exit(0);
+
+
+	}//main
 
 }//Class
 
