c2d5c15a847fc8695692e69244b283f04577d2e8
Magnus Forsberg <magnus.forsberg@gmail.com>
2016-06-21 14:40:44 +0200

    Flyttade CLI till ny class CLI.

---
+++ JavaKursPHsystem/src/PHUtskrift/CLI.java
@@ -0,0 +1,43 @@
+package PHUtskrift;
+
+import java.util.Scanner;
+
+abstract class CLI {
+
+	static void visaCLI(){
+		Scanner scanner = new Scanner(System.in);
+		String input = "";
+		char kommando = '#';
+
+		while(true){
+			System.out.println("\nVälj ett kommando:");
+			System.out.println("==================");
+
+			System.out.println("1. Skapa ny anställd");
+			System.out.println("\n0. Avsluta");
+
+			do{
+				input = scanner.nextLine().trim();
+			}while(input.length() == 0);
+			//input är nu garanterat icke-tom
+
+			kommando = input.charAt(0);
+			switch(kommando) {
+			case '1':
+				System.out.println("skapa ny anställd");
+				break;
+			
+			case '0':
+				scanner.close();
+				return;
+				
+			default:
+				System.out.println("Okänt kommando");
+			break;
+			}//switch
+
+		}//while
+
+
+	}
+}
--- JavaKursPHsystem/src/PHUtskrift/MainPH.java
+++ JavaKursPHsystem/src/PHUtskrift/MainPH.java
@@ -1,42 +1,13 @@
 package PHUtskrift;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class MainPH {
 
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-
-		int input = -1;
-
-		while(input != 0) {
-			System.out.println("\nVälj ett kommando:");
-			System.out.println("==================");
-
-			System.out.println("1. Skapa ny anställd");
-
-			System.out.println("\n0. Avsluta");
-
-			try{
-				input = scanner.nextInt();
-			} catch (InputMismatchException ime) {
-				System.out.println("input: " + input);
-				System.out.println("Endast siffror.");
-				
-			}
-
-			scanner.nextLine();
-			System.out.println(input);
-
-		}//while
-
+		CLI.visaCLI();
 		System.out.println("\nProgrammet avslutas.");
-		scanner.close();
-		System.exit(0);
-
 
 	}//main
 
